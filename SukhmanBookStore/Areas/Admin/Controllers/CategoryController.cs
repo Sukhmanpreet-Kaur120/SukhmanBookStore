331b6e4a678dd6bccff2b7e3978e1e9979973d03
W0826062 <W0826062@myscc.ca>
2023-11-06 21:33:41 -0500

    update

--- SukhmanBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ SukhmanBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -12,6 +12,7 @@ namespace SukhmanBookStore.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
         public CategoryController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -37,15 +38,50 @@ namespace SukhmanBookStore.Areas.Admin.Controllers
             }
             return View(category);
         }
-        #region
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+        //API Calls
+        #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
             var allObj = _unitOfWork.Category.GetAll();
             return Json(new { data = allObj });
         }
-        #endregion
-        
 
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleteing" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+        #endregion
     }
+
 }
